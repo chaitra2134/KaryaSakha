0fb95c0b0e23f013860310475ce33e2da9d45e0c
Prizam-Sarkhi <prizam0170477@gmail.com>
2023-09-23 21:59:44 +0530

    added calendar

---
+++ KaryaSakha_Integrated_4.0/Calendar/deleteEvent.php
@@ -0,0 +1,29 @@
+<?php       
+
+    /*
+        Author: Chinmayee
+        Date: 24/08/2023
+    */
+    
+    include '../Tables_and_Connection/Connection.php';
+    session_start();         
+
+    $data = file_get_contents("php://input");
+    $jdata = json_decode($data, true);
+
+    $userId = $_SESSION['userId'];
+    $eventId = $jdata['eventId'];
+
+
+    if(!empty($eventId))
+    {
+        $sql = "DELETE FROM Calender WHERE eventId = $eventId && userId = $userId";
+        if($conn->query($sql) === TRUE)
+            echo "Event Deleted Successfully!";
+        else 
+            echo "Error Deleting Event!";
+    }
+    else 
+        echo "No Such Event Found!";
+
+?>
